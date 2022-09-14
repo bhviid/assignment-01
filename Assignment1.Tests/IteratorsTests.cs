96d9daad9c563a5b7f9d7e414cf9b50deb424c2d
Anton Vitus Sohl Andersen <anton.sohl@gmail.com>
2022-09-14 15:25:29 +0200

    Changed tests to arrange-act-assert

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -7,26 +7,42 @@ public class IteratorsTests
     [Fact]
     public void Flatten_When_StreamsOfStreams_Returns_FlatStream()
     {
-        
-       IEnumerable<IEnumerable<string>> e =  new List<List<string>>() { new List<string>(){"a","b"}, new List<string>(){"c","d"}};
-       Assert.Equal(4,Iterators.Flatten(e).Count());
-       
+        // Arrange 
+        IEnumerable<IEnumerable<string>> e = new List<List<string>>() { new List<string>() { "a", "b" }, new List<string>() { "c", "d" } };
+
+        // Act 
+        var flatten = Iterators.Flatten(e).Count();
+
+        // Assert
+        Assert.Equal(4, flatten);
     }
-    
+
     [Fact]
     public void Flatten_When_StreamsOfStreams_Returns_OriginalElements()
     {
-        IEnumerable<IEnumerable<string>> e =  new List<List<string>>() { new List<string>(){"a","b"}, new List<string>(){"c","d"} };
-        var f = Iterators.Flatten(e);
-        Assert.Contains("a", f);
-        Assert.Contains("b", f);
-        Assert.Contains("c", f);
-        Assert.Contains("d", f);
+        // Arrange 
+        IEnumerable<IEnumerable<string>> e = new List<List<string>>() { new List<string>() { "a", "b" }, new List<string>() { "c", "d" } };
+
+        // Act 
+        var flatten = Iterators.Flatten(e);
+
+        // Assert
+        Assert.Contains("a", flatten);
+        Assert.Contains("b", flatten);
+        Assert.Contains("c", flatten);
+        Assert.Contains("d", flatten);
     }
+
     [Fact]
     public void Filter_When_Stream_Returns_FilteredStream()
     {
-        IEnumerable<string> e = new List<string>(){"a", "b"};
-        Assert.Single(Iterators.Filter(e, s => s.Equals("a")));
+        // Arrange
+        IEnumerable<string> e = new List<string>() { "a", "b" };
+
+        // Act
+        var filter = Iterators.Filter(e, s => s.Equals("a"));
+
+        // Assert 
+        Assert.Single(filter);
     }
 }
